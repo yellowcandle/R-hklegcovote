91eaea0f3cdead246b3dbbe61ff447b0e588e726
yellowcandle <herballemon@gmail.com>
2014-04-26 12:26:50 +0800

    labeling and changed plot theme

--- plot.R
+++ plot.R
@@ -5,5 +5,19 @@ names <- legco.members.info
 svddata <- svd(x[,3:72],nu=2,nv=2)
 # do data frame
 dataf <- data.frame(members=names$Members,affi=names$Political.Affiliation,bloc=names$Political.Bloc,v1=svddata$v[,1],v2=svddata$v[,2])
+# labeling for DAB
+dataf$DAB = dataf$members
+View(dataf)
+idx = (dataf$affi=="DAB")
+dataf$DAB[!idx]=NA
+# labeling for PP
+dataf$PP = dataf$members
+View(dataf)
+idx = (dataf$affi=="PP")
+dataf$PP[!idx]=NA
 # plot with ggplot2
-ggplot(dataf,aes(x=v1,y=v2,color=bloc))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point()
+ggplot(dataf,aes(x=v1,y=v2,color=bloc))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point()+geom_text(size=3,hjust=0,alpha=0.5)+theme_bw()
+# plot with only DAB members labeled
+ggplot(dataf,aes(x=v1,y=v2,color=bloc,label=DAB))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point()+geom_text(size=3,hjust=0,alpha=0.5)+theme_bw()
+# plot with only PP members labeled
+ggplot(dataf,aes(x=v1,y=v2,color=bloc,label=PP))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point()+geom_text(size=3,hjust=0,alpha=0.5)+theme_bw()
