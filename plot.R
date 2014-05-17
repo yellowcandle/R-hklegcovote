63c0bccc6f83ed5e40f57d944fe7dfab57215f03
Stephen Wong <herballemon@gmail.com>
2014-05-17 10:21:06 +0800

    New labeling for plot.

--- plot.R
+++ plot.R
@@ -1,3 +1,6 @@
+# load packages
+library("ggplot2")
+
 # read data
 x <- legco.division.20121017.20140416.csv.numeric.csv
 names <- legco.members.info
@@ -13,15 +16,13 @@ dataf$DAB[!idx]=NA
 dataf$PP = dataf$members
 idx = (dataf$affi=="PP")
 dataf$PP[!idx]=NA
+#labeling for DP
+dataf$DP = dataf$members
+idx = (dataf$affi=="DP")
+dataf$DP[!idx]=NA
 # plot with ggplot2
-ggplot(dataf,aes(x=v1,y=v2,color=bloc))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point(alpha=0.5)+geom_text(size=3,hjust=0)+theme_bw()
+ggplot(dataf,aes(x=v1,y=v2,color=bloc))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point()+theme_bw()
 # plot with only DAB members labeled
-ggplot(dataf,aes(x=v1,y=v2,color=bloc,label=DAB))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point()+geom_text(size=3,hjust=0)+theme_bw()
+ggplot(dataf,aes(x=v1,y=v2,color=bloc,label=DAB))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point(alpha=0.5)+geom_point(size=3,data=subset(dataf,affi=="DAB"))+theme_bw()
 # plot with only PP members labeled
-ggplot(dataf,aes(x=v1,y=v2,color=bloc,label=PP))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point()+geom_text(size=3,hjust=0)+theme_bw()
-# highlighting only DAB members
-ggplot(dataf,aes(x=v1,y=v2,color=bloc))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point(alpha=0.3)+geom_point(data=subset(dataf, affi=="DAB"))+theme_bw()
-# highlighting only CP members
-ggplot(dataf,aes(x=v1,y=v2,color=bloc))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point(alpha=0.3)+geom_point(data=subset(dataf, affi=="CP"))+theme_bw()
-# highlighting only LAB members with larger dots
-gplot(dataf,aes(x=v1,y=v2,color=bloc))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point(alpha=0.3)+geom_point(data=subset(dataf, affi=="LAB"),size=3)+theme_bw()
+ggplot(dataf,aes(x=v1,y=v2,color=bloc,label=DAB))+xlim(-0.2,0.2)+ylim(-0.25,0.25)+geom_point(alpha=0.5)+geom_point(size=3,data=subset(dataf,affi=="PP"))+theme_bw()
