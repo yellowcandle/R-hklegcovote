06104f394703bbf4bd1a516de666e0931635a4aa
yellowcandle <herballemon@gmail.com>
2014-08-03 00:46:10 +0800

    Updated plotting of subset

--- plot.R
+++ plot.R
@@ -34,4 +34,4 @@ ggplot(dataf,aes(x=V1,y=V2,color=Political.Bloc,label=DAB))+geom_point(alpha=0.5
 ggplot(dataf,aes(x=V1,y=V2,color=Political.Bloc,label=PP))+geom_point(alpha=0.5)+geom_point(size=3,data=subset(dataf,Political.Affiliation=="PP"))+theme_bw()+xlim(-.25,.25)+ylim(-.25,.25)
 # plot with names
 ggplot(dataf,aes(x=V1,y=V2,color=Political.Bloc,label=Members))+geom_point(alpha=0)+geom_text()+theme_bw()+xlim(-.25,.25)+ylim(-.25,.25)
-ggplot(subset(dataf,Political.Bloc="D"),aes(x=V1,y=V2,color=Political.Bloc,label=Members))+geom_point(alpha=0)+geom_text()+theme_bw()+xlim(-.25,.25)+ylim(-.25,.25)
+ggplot(subset(dataf,Political.Bloc=="D"),aes(x=V1,y=V2,color=Political.Affiliation,label=Members))+geom_point(alpha=0)+geom_text(size=4)+theme_bw()
