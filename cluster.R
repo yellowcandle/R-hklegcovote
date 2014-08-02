5c3291cb1dd551dbad3cb2ef5cda0c420f2feb45
yellowcandle <herballemon@gmail.com>
2014-08-03 00:06:29 +0800

    Updated clustering code

--- cluster.R
+++ cluster.R
@@ -1,11 +1,15 @@
-# read data
-x <- legco.division.20121017.20140416.csv.numeric.csv
-names <- legco.members.info
-# run SVD
-svddata <- svd(x[,3:72],nu=2,nv=2)
-# do data frame
-dataf <- data.frame(members=names$Members,affi=names$Political.Affiliation,bloc=names$Political.Bloc,v1=svddata$v[,1],v2=svddata$v[,2])
+# do SVD
+svd <- svd(votedfv[,3:72],nu=0,nv=2)
+svdv <- svd$v
+rownames(svdv) <- c( "TSANG Yok-sing", "Albert HO", "LEE Cheuk-yan", "James TO", "CHAN Kam-lam", "LEUNG Yiu-chung", "Dr LAU Wong-fat", "Emily LAU", "TAM Yiu-chung", "Abraham SHEK", "Tommy CHEUNG", "Frederick FUNG", "Vincent FANG", "WONG Kwok-hing", "Dr Joseph LEE", "Jeffrey LAM", "Andrew LEUNG", "WONG Ting-kwong", "Ronny TONG", "Cyd HO", "Starry LEE", "Dr LAM Tai-fai", "CHAN Hak-kan", "CHAN Kin-por", "Dr Priscilla LEUNG", "Dr LEUNG Ka-lau", "CHEUNG Kwok-che", "WONG Kwok-kin", "IP Kwok-him", "Mrs Regina IP", "Paul TSE", "Alan LEONG", "LEUNG Kwok-hung", "Albert CHAN", "WONG Yuk-man", "Claudia MO", "Michael TIEN", "James TIEN", "NG Leung-sing", "Steven HO", "Frankie YICK", "WU Chi-wai", "YIU Si-wing", "Gary FAN", "MA Fung-kwok", "Charles Peter MOK", "CHAN Chi-chuen", "CHAN Han-pan", "Dr Kenneth CHAN", "CHAN Yuen-han", "LEUNG Che-cheung", "Kenneth LEUNG", "Alice MAK", "Dr KWOK Ka-ki", "KWOK Wai-keung", "Dennis KWOK", "Christopher CHEUNG", "Dr Fernando CHEUNG", "SIN Chung-kai", "Dr Helena WONG", "IP Kin-yuen", "Dr Elizabeth QUAT", "Martin LIAO", "POON Siu-ping", "TANG Ka-piu", "Dr CHIANG Lai-wan", "Ir Dr LO Wai-kwok", "CHUNG Kwok-pan", "Christopher CHUNG", "Tony TSE" )
+
+# read legco members data
+legco.members.info <- read.csv("legco-members-info.csv")
+
+# merge data
+dataf <- merge (legco.members.info, svdv, by.x="Members", by.y="row.names")
+
 # do cluster
 hc <- hclust(dist(dataf))
 #plot cluster
-plot(hc,labels=dataf$members, hang = -1, cex=0.5)
+plot(hc,labels=dataf$Members, hang = -1, cex=0.7)
